5e73e3d1638ca68f63346e5cd56d7258f2f1eb3a
thakoraarya <72544429+thakoraarya@users.noreply.github.com>
2023-06-23 23:46:46 +0000

    env inception

--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
-
+// import {config} from 'dotenv'
+// dotenv.config();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-3kdbhzzujbs1kvgj.us.auth0.com"
-      clientId="XUSjqhjJCGNp9Lia9Ayey230k6LXOCh9"
-      authorizationParams={{
-        redirect_uri: window.location.origin
-      }}>
-      <App />
-    </Auth0Provider>
-  </React.StrictMode>
+      domain='process.env.REACT_APP_DOMAIN'
+    clientId='process.env.REACT_APP_CLIENDID'
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
+    <App />
+  </Auth0Provider>
+  </React.StrictMode >
 );
 
 // If you want to start measuring performance in your app, pass a function
