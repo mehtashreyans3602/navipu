8bc1f1abffacf63373dffcb937a5027f4064cc72
thakoraarya <aarya191102@gmail.com>
2023-06-29 10:56:27 +0530

    issue in usecontext

--- src/components/AdminMap.jsx
+++ src/components/AdminMap.jsx
@@ -7,7 +7,7 @@ import ControllPanel from './atoms/ControllPanel';
 
 
 const center = {
-  lat:  22.288540,
+  lat: 22.288540,
   lng: 73.364620
 }
 
@@ -15,17 +15,22 @@ const center = {
 const AdminMap = () => {
 
   const [map, setMap] = useState( /** @type google.maps.map */ null);
+  const [selectedLocation, setSelectedLocation] = useState(null);
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
   });
 
+  const handleCardClick = (location) => {
+    setSelectedLocation(location);
+  };
+
 
   const handleMapClick = (event) => {
     const lat = event.latLng.lat();
     const lng = event.latLng.lng();
-
-    // Do something with the latitude and longitude values
+    setSelectedLocation({ lat, lng });
+    // Do something with the latitude and  valueslongitude
     console.log('Clicked coordinates:', lat, lng);
   };
 
@@ -41,14 +46,14 @@ const AdminMap = () => {
     <section className='w-screen h-screen flex flex-col gap-2 md:gap-6 overflow-hidden'>
       <Header />
       <div className='w-full h-[100%] flex gap-2 md:gap-6'>
-        <Nearby />
+        <Nearby  />
         <div className='flex w-[70%] h-full md:flex-col gap-y-2 md:gap-y-6 pb-2 md:pb-6 flex-col-reverse'>
           <ControllPanel />
-          <GoogleMap onClick={handleMapClick} mapContainerClassName='w-full h-full rounded-3xl relative' center={center} zoom={16} options={{ fullscreenControl: false, zoomControl: false, streetViewControl: false, map }}  onLoad={map => setMap(map)}>
+          <GoogleMap onClick={handleMapClick} mapContainerClassName='w-full h-full rounded-3xl relative' center={center} zoom={16} options={{ fullscreenControl: false, zoomControl: false, streetViewControl: false, map }} onLoad={map => setMap(map)}>
             {/* display marker and directions */}
             <button onClick={() => map.panTo(center)} className="material-symbols-outlined text-xl absolute bg-green-400 p-2 m-4 rounded-3xl right-0 top-0 "> my_location
             </button>
-            <Marker  position={center}  />
+            <Marker position={center} />
           </GoogleMap>
 
         </div>
