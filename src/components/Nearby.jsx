c54e63b6160cc8249468ed9048f2c9906d2539ef
thakoraarya <aarya191102@gmail.com>
2023-07-01 08:20:15 +0530

    marker not updating

--- src/components/Nearby.jsx
+++ src/components/Nearby.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import NearbyCard from "./atoms/NearbyCard";
+import { LocationProvider } from '../context/Locationcontext';
 const Nearby = () => {
   return (
     <div className='overflow-hidden flex flex-col gap-2 w-[15%] md:w-[10%] h-full rounded-tr-3xl bg-blue-200 p-[1.7%] max-h-[800px]'>
       <p className='font-sans font-semibold uppercase text-xs flex items-center'> Nearby</p>
       <section className='h-[100%]  scroll-smooth rounded-md md:rounded-xl w-full flex flex-col gap-2 md:gap-4 overflow-y-scroll'>
 
-
-        <NearbyCard  lat="22.28878772337861" lng="73.36346800799352" />
+        <LocationProvider>
+          <NearbyCard placename='Some Place' lat={22.286993880495107} lng={73.36407050556579} />
+          {/* Add more NearbyCard components here */}
+        </LocationProvider>
 
 
 
         {/* <NearbyCard placename='PIET' lat="22.28878772337861" lng="73.36346800799352" />
+        <NearbyCard  lat="22.28878772337861" lng="73.36346800799352" />
         <NearbyCard placename='PIT' />
         <NearbyCard placename='PIA' />
         <NearbyCard placename='PIBA' />
