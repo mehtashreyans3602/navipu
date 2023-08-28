fbd3f8072c5272d6be8d56d27a0dba492c0ed7fe
thakoraarya <aarya191102@gmail.com>
2023-08-28 12:05:02 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import LoginButton from './components/login';
 import AdminMap from './components/AdminMap';
-import { LocationProvider } from "./context/Locationcontext";
 import Guestmap from "./components/Guestmap";
 import { useAuth0 } from "@auth0/auth0-react";
 
@@ -22,13 +21,13 @@ function App() {
 
 
       {!isAuthenticated ? <LoginButton logintext='Admin Entry' /> : ''}
-      <LocationProvider>
+
         {/*{Auth() ? <h1>{user.name}</h1> : <h2>invalid user</h2>}*/}
         {Auth() ? console.table(user) : console.log('invalid user')}
 
         {/* <AdminMap /> */}
         {Auth() ? <AdminMap /> : <Guestmap />}
-      </LocationProvider>
+
     </div>
   );
 }
