189f98eb7dd01f0bea63e4b9a4bb356f2511aad5
thakoraarya <aarya191102@gmail.com>
2023-09-12 04:30:37 +0530

    fixes and improvements

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 // import logo from './logo.svg';
-import './App.css';
-import LoginButton from './components/login';
-import AdminMap from './components/AdminMap';
-import { LocationProvider } from "./context/Locationcontext";
-import Guestmap from "./components/Guestmap";
-import { useAuth0 } from "@auth0/auth0-react";
+import "./App.css";
+import AdminMap from "./components/AdminMap";
+// import LoginButton from './components/login';
+// import Guestmap from "./components/Guestmap";
+// import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  const { isAuthenticated, user } = useAuth0();
+  // const { isAuthenticated, user } = useAuth0();
 
-  function Auth() {
-    // these are the allowed accounts for admin add your sub
-    const SubAcc = [
-      'google-oauth2|106746765731439499987',
-    ]
-    return isAuthenticated && SubAcc.includes(user.sub);
-  }
+  // function Auth() {
+  //   // these are the allowed accounts for admin add your sub
+  //   const SubAcc = [
+  //     'google-oauth2|106746765731439499987',
+  //   ]
+  //   return isAuthenticated && SubAcc.includes(user.sub);
+  // }
 
   return (
     <div className="App w-full h-screen">
+      {/*{!isAuthenticated ? <LoginButton logintext='Admin Entry' /> : ''}*/}
 
+      {/*{Auth() ? <h1>{user.name}</h1> : <h2>invalid user</h2>}*/}
+      {/*{Auth() ? console.table(user) : console.log('invalid user')}*/}
 
-      {!isAuthenticated ? <LoginButton logintext='Admin Entry' /> : ''}
-      <LocationProvider>
-        {/*{Auth() ? <h1>{user.name}</h1> : <h2>invalid user</h2>}*/}
-        {Auth() ? console.table(user) : console.log('invalid user')}
-
-        {/* <AdminMap /> */}
-        {Auth() ? <AdminMap /> : <Guestmap />}
-      </LocationProvider>
+      <AdminMap />
+      {/*{Auth() ? <AdminMap /> : <Guestmap />}*/}
     </div>
   );
 }
