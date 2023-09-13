80001d78ba54524acada0882e24d6c02b57b3bd7
thakoraarya <aarya191102@gmail.com>
2023-09-13 15:44:26 +0530

    nearby card added with place detail onclick

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -5,12 +5,14 @@ import EventsCard from "./atoms/EventsCard";
 const Events = () => {
   return (
     <div className=" w-[20%] h-full rounded-tl-3xl overflow-y-auto bg-[#fcf9f1] border-[#78786a] flex flex-col items-center gap-y-6 border-2 border-r-0 border-b-0 p-[1.7%]">
-      <p className="font-mono font-medium uppercase text-2xl flex items-center gap-x-4">
+      <div className="font-mono font-medium uppercase text-2xl flex items-center gap-x-4">
         <span className="material-symbols-outlined font-semibold text-2xl w-fit">
           celebration
-        </span>{" "}
-        Events
-      </p>
+        </span>
+        <span className=" w-auto font-mono font-medium uppercase text-2xl">
+          Events
+        </span>
+      </div>
 
       {events.map((event) => (
         <EventsCard
