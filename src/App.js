9632e0f3424328eadd5d9be3d67ab2415a1dbee6
thakoraarya <aarya191102@gmail.com>
2023-09-13 15:50:01 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -16,12 +16,6 @@ function App() {
 
   return (
     <div className="App w-full h-screen flex justify-center items-center p-0 m-0">
-      {/*{!isAuthenticated ? <LoginButton logintext='Admin Entry' /> : ''}*/}
-
-      {/*{Auth() ? <h1>{user.name}</h1> : <h2>invalid user</h2>}*/}
-      {/*{Auth() ? console.table(user) : console.log('invalid user')}*/}
-
-      {/*<AdminMap />*/}
       {Auth() ? <AdminMap /> : <LoginButton logintext="Admin Entry" />}
     </div>
   );
