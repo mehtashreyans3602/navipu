179a53c1c28fcc8365fdf4dd8d69cfb3dfbb96fd
thakoraarya <aarya191102@gmail.com>
2023-09-17 12:05:03 +0530

    build fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 // import logo from './logo.svg';
 import "./App.css";
-import LoginButton from "./components/login";
+// import LoginButton from "./components/login";
 import AdminMap from "./components/AdminMap";
 // import Guestmap from "./components/Guestmap";
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  const { isAuthenticated, user } = useAuth0();
+  // const { isAuthenticated, user } = useAuth0();
 
-  function Auth() {
-    // these are the allowed accounts for admin add your sub
-    const SubAcc = ["google-oauth2|106746765731439499987"];
-    return isAuthenticated && SubAcc.includes(user.sub);
-  }
+  // function Auth() {
+  //   // these are the allowed accounts for admin add your sub
+  //   const SubAcc = ["google-oauth2|106746765731439499987"];
+  //   return isAuthenticated && SubAcc.includes(user.sub);
+  // }
 
   return (
     <div className="App w-full h-screen flex justify-center items-center p-0 m-0">
-      {Auth() ? <AdminMap /> : <LoginButton logintext="Admin Entry" />}
+      {/*{Auth() ? <AdminMap /> : <LoginButton logintext="Admin Entry" />}*/}
+      <AdminMap />
     </div>
   );
 }
