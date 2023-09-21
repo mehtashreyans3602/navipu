14a71059839675b9669f875f450e08d0da92768b
Shreyans <103171711+mehtashreyans3602@users.noreply.github.com>
2023-09-22 00:36:47 +0530

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <link rel="icon" href="https://yt3.googleusercontent.com/oEQpDQ-Mt5ICmTvrKelavxxPI1Pk2i3dt0JM1fypJvylX1SEvkVjAeqOhwXNOeQK9k3RSWY5ZQ=s176-c-k-c0x00ffffff-no-rj" />
-    
+      <title>PU Nav</title>
       </Head>
       <body>
         <Main />
